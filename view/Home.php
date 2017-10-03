cb0fd1047637a14161b8c2aced7227ccbc22e300
Gabriel Brañeiro <gabbraneiro@gmail.com>
2017-10-03 15:00:56 -0300

    sigue prueba

--- view/Home.php
+++ view/Home.php
@@ -11,8 +11,9 @@ class Home extends TwigView {
     
     public function show() {
         
-        echo self::getTwig()->render('home.html.twig');
-        
+        echo self::getTwig()->render('head.html.twig');
+        echo self::getTwig()->render('container.html.twig','index');
+        echo self::getTwig()->render('footer.html.twig');	        
         
     }
     
