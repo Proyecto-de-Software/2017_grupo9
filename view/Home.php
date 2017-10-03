82180e42182900025bb31715554059a7c509cf6c
Gabriel Brañeiro <gabbraneiro@gmail.com>
2017-10-03 16:05:31 -0300

    head

--- view/Home.php
+++ view/Home.php
@@ -11,7 +11,7 @@ class Home extends TwigView {
     
     public function show() {
         
-        echo self::getTwig()->render('head.twig.html');
+        echo self::getTwig()->render('head.twig.html',  array('title' => 'Hospital Gutierrez'));
         echo self::getTwig()->render('container.twig.html', array('tipo' => 'index'));
         echo self::getTwig()->render('footer.twig.html');	        
         
