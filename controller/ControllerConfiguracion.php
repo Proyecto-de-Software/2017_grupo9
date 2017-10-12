8229e314ce4f19aca685e2108f046f3034170508
Juan Schällibaum <juanschallibaum@hotmail.com>
2017-10-12 04:27:26 -0300

    Actualizado controller configuracion

--- controller/ControllerConfiguracion.php
+++ controller/ControllerConfiguracion.php
@@ -3,8 +3,8 @@
 	require_once($_SERVER['DOCUMENT_ROOT']."/model/ClaseConfiguracion.php");
 
 	function crearConfiguracion(){
-		
-		return new Configuracion($_POST['titulo'], $_POST['descripcion'], $_POST['email'], $_POST['elementos'], $_POST['habilitado']);
+
+		return new Configuracion(NULL, $_POST['titulo'], $_POST['descripcion'], $_POST['email'], $_POST['elementos'], $_POST['habilitado']);
 	}
 
 	RepositorioConfiguracion::getInstance()->modificarConfiguracionHospital(crearConfiguracion());
