11b60d6865a7814df52fdfb206b1fdc0aee9aaa6
Gabriel Brañeiro <gabbraneiro@gmail.com>
2017-11-15 17:34:56 -0300

    asdasdsa

---
+++ controller/Controllers viejos/ControllerPaciente.php
@@ -0,0 +1,203 @@
+<?php
+	
+	#Nos ubicamos en el document_root para evitar problemas al usar twig, ya que twig usa paths relativos y si no estamos en la raiz no funciona.
+	chdir($_SERVER['DOCUMENT_ROOT']);
+
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/ClasePaciente.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioPaciente.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioConfiguracion.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioUsuario.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioRol.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioPermiso.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/ClaseUsuario.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/view/TwigView.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/controller/ControllerSeguridad.php");
+
+	if(!isset($_SESSION)) {
+		sec_session_start();
+	} else {
+		session_regenerate_id();
+	}
+
+	function obtenerConfiguracion(){
+    	return RepositorioConfiguracion::getInstance()->datosParaLaVista();
+    }
+
+    $config = obtenerConfiguracion();
+	if(!$config['habilitado']){
+	    header("Location: /../");
+	}
+
+	function usuarioActual(){
+    	if(isset($_SESSION['idUsuario'])){
+    		$usuario = RepositorioUsuario::getInstance()->buscarUsuarioPorId($_SESSION['idUsuario']);
+    		return array(	'logueado'=>true, 
+    						'username'=>$usuario->getNombreUsuario(),
+    						'roles'=>RepositorioRol::getInstance()->buscarRolesDeUsuario($_SESSION['idUsuario']),
+    						'idUsuario'=>$_SESSION['idUsuario'],
+    						'token'=>$_SESSION['token']
+    					);
+    	}
+    	else return false;
+    }
+
+
+	function crearPaciente(){
+		$paciente = new Paciente($_POST['apellido'], $_POST['nombre'], $_POST['domicilio'], $_POST['telefono'], $_POST['fechaNacimiento'], $_POST['genero'], $_POST['idObraSocial'], $_POST['idTipoDocumento'], $_POST['numeroDoc']);
+		return $paciente;
+	}
+
+	function listarPacientes($pacientes){
+	    echo TwigView::getTwig()->render('administracionPacientes.twig', array('usuarioActual' => usuarioActual(),'lista'=>$pacientes,'configuracion'=>obtenerConfiguracion(), 'paginado' => datosDePaginado()));
+   	}
+
+    function mostrarPaciente($paciente,$obraSocial,$tipoDeDocumento){
+	    echo TwigView::getTwig()->render('administracionMostrarPaciente.twig', array('usuarioActual' => usuarioActual(), 'paciente' => $paciente, 'tipoDeDocumento' => $tipoDeDocumento, 'obraSocial' => $obraSocial, 'configuracion'=>obtenerConfiguracion()));
+    }
+
+    function agregarPaciente($obrasSociales,$tiposDeDocumento,$validacion=[]){ //paranetro validacion = null
+	    echo TwigView::getTwig()->render('administracionAgregarPaciente.twig', array('usuarioActual' => usuarioActual(), 'validacion'=>$validacion, 'tiposDeDocumento' => $tiposDeDocumento, 'obrasSociales' => $obrasSociales, 'configuracion'=>obtenerConfiguracion()));
+    }
+
+    function modificarPaciente($paciente, $obrasSociales, $tiposDeDocumento,$validacion=[]){
+	    echo TwigView::getTwig()->render('administracionModificarPaciente.twig', array('usuarioActual' => usuarioActual(), 'paciente' => $paciente, 'validacion'=>$validacion, 'tiposDeDocumento' => $tiposDeDocumento, 'obrasSociales' => $obrasSociales, 'configuracion'=>obtenerConfiguracion()));
+    } 
+
+    function datosDePaginado(){
+
+    	$cantidadPacientes = (int)RepositorioPaciente::getInstance()->cantidadDePacientes();
+    	$cantidadPorPagina = (int)RepositorioConfiguracion::getInstance()->obtenerDatosDeConfiguracion()->getCantElem();
+    	$cantidadDePaginas = ceil($cantidadPacientes/$cantidadPorPagina);
+    	$actual = 1;
+    	if(isset($_GET['page'])){
+    		$actual = $_GET['page'];
+    	}
+    	if($actual <= 1){
+    		$limit = 0;
+    	}
+    	else{
+    		$limit = $cantidadPorPagina * ($actual - 1);
+    	}
+    	
+    	$retorno = array(
+    		'cantidadPorPagina' => $cantidadPorPagina,
+    		'cantPaginas' =>  $cantidadDePaginas,
+    		'actual' => $actual,
+    		'limit' => $limit
+    			);
+
+    	return $retorno;
+    }
+
+	if(isset($_GET['action'])){
+		switch ($_GET['action']) {
+			case 'altaDePaciente': //Copiado
+				if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_new')) {
+					$obrasSociales = RepositorioPaciente::getInstance()->devolverObrasSociales();
+		    		$tiposDeDocumento = RepositorioPaciente::getInstance()->devolverTiposDeDocumento();
+		       		agregarPaciente($obrasSociales,$tiposDeDocumento);
+		       	} else {
+	    			header("Location: /../");
+	    		}
+		       	break;
+		    case 'agregarPaciente': //Copiado
+		    	if((RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_new')) && (isset($_POST['token']) && $_POST['token'] == $_SESSION['token'])) {
+		    		$paciente = crearPaciente();
+		    		$validacion = RepositorioPaciente::getInstance()->pacienteValido($paciente);
+		    		if ($validacion['ok']){
+						if(RepositorioPaciente::getInstance()->agregarPaciente($paciente)){
+	        				$id = $paciente->getId();
+			    			header("location: /../controller/ControllerPaciente.php/?action=mostrarPaciente&id=$id");
+			       		}
+			       		else{
+			    			header("location: /../controller/ControllerPaciente.php/?action=altaDePaciente");
+			       		}
+			       	} else{		       		
+			    		$obrasSociales = RepositorioPaciente::getInstance()->devolverObrasSociales();
+		    			$tiposDeDocumento = RepositorioPaciente::getInstance()->devolverTiposDeDocumento();
+						agregarPaciente($obrasSociales,$tiposDeDocumento,$validacion);
+					}
+			       	
+		       	} else {
+	    			header("Location: /../");
+	    		}
+		        break;
+		    case 'modificacionDePaciente': //Copiado
+		    	if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_update')){
+			    	$paciente = RepositorioPaciente::getInstance()->buscarPacientePorId($_GET['id']);
+			    	$obrasSociales = RepositorioPaciente::getInstance()->devolverObrasSociales();
+			    	$tiposDeDocumento = RepositorioPaciente::getInstance()->devolverTiposDeDocumento();
+			    	modificarPaciente($paciente, $obrasSociales, $tiposDeDocumento);
+			    } else {
+	    			header("Location: /../");
+	    		}
+		    	break;
+		    case 'modificarPaciente': //Copiado
+		    	if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_update')){
+		    		$paciente = crearPaciente();
+		    		$paciente->setId($_GET['id']);
+		    		$validacion = $validacion = RepositorioPaciente::getInstance()->pacienteValido($paciente,true);
+		    		$id = $paciente->getId();
+					if($validacion['ok']){
+			    		if(RepositorioPaciente::getInstance()->modificarPaciente($paciente)){
+			    			header("location: /../controller/ControllerPaciente.php/?action=mostrarPaciente&id=$id");
+		        		}
+		        	}
+		        	else {
+		        		$paciente = RepositorioPaciente::getInstance()->buscarPacientePorId($_GET['id']);
+				    	$obrasSociales = RepositorioPaciente::getInstance()->devolverObrasSociales();
+				    	$tiposDeDocumento = RepositorioPaciente::getInstance()->devolverTiposDeDocumento();
+				    	modificarPaciente($paciente, $obrasSociales, $tiposDeDocumento,$validacion);
+		        	}
+		        } else {
+	        		header("Location: /../");
+    			}
+		        break;
+		    case 'eliminarPaciente': //Copiado
+		   		if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_destroy')){
+		        	if(RepositorioPaciente::getInstance()->eliminarPaciente($_GET['id'])){
+		        		header("location: /../controller/ControllerPaciente.php/?action=listarPacientes");
+						$paginado = datosDePaginado();
+		        		listarPacientes(RepositorioPaciente::getInstance()->devolverPacientes($paginado['limit'],$paginado['cantidadPorPagina']));
+		        	}
+		        } else {
+	        		header("Location: /../");
+    			}
+		        break;
+		    case 'mostrarPaciente': //Copiado
+		    	if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_show')){
+		    		$paciente = RepositorioPaciente::getInstance()->buscarPacientePorId($_GET['id']);
+		    		$obraSocial = RepositorioPaciente::getInstance()->devolverObraSocialPorId($paciente->getIdObraSocial());
+		    		$tipoDeDocumento = RepositorioPaciente::getInstance()->devolverTipoDeDocumentoPorId($paciente->getIdTipoDocumento());
+		    		mostrarPaciente($paciente,$obraSocial,$tipoDeDocumento);
+		        } else {
+	        		header("Location: /../");
+    			}
+		        break;
+		    case 'listarPacientes': //Copiado
+		    	if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_index')){
+					$paginado = datosDePaginado();
+		        	listarPacientes(RepositorioPaciente::getInstance()->devolverPacientes($paginado['limit'],$paginado['cantidadPorPagina']));
+		        } else {
+	        		header("Location: /../");
+    			}
+		        break;
+		     case 'busquedaNomYAp': //Copiado
+		     	if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_index')){
+					$paginado = datosDePaginado();
+		     		listarPacientes(RepositorioPaciente::getInstance()->devolverPacientes($paginado['limit'],$paginado['cantidadPorPagina'],$_POST['busquedaNombre'],$_POST['busquedaApellido']));
+		     	} else {
+	        		header("Location: /../");
+    			}
+    			break;
+			case 'busquedaDocumento': //Copiado
+		     	if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'paciente_index')){
+					$paginado = datosDePaginado();
+		     		listarPacientes(RepositorioPaciente::getInstance()->devolverPacientes($paginado['limit'],$paginado['cantidadPorPagina'],'','',$_POST['busquedaTipoDoc'],$_POST['busquedaNumeroDoc']));
+		     	} else {
+	        		header("Location: /../");
+    			}
+    			break;
+		}
+	}	
+
---
+++ controller/Controllers viejos/ControllerUsuario.php
@@ -0,0 +1,326 @@
+<?php
+
+	#Nos ubicamos en el document_root para evitar problemas al usar twig, ya que twig usa paths relativos y si no estamos en la raiz no funciona.
+	chdir($_SERVER['DOCUMENT_ROOT']);
+	
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioUsuario.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioRol.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioPermiso.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/RepositorioConfiguracion.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/ClaseConfiguracion.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/model/ClaseUsuario.php");
+	require_once($_SERVER['DOCUMENT_ROOT'].'/view/TwigView.php');
+	require_once($_SERVER['DOCUMENT_ROOT']."/controller/ControllerSeguridad.php");
+
+	if(!isset($_SESSION)) {
+		sec_session_start();
+	} else {
+		session_regenerate_id();
+	}
+
+	function obtenerConfiguracion(){
+    	return RepositorioConfiguracion::getInstance()->datosParaLaVista();
+    }
+    $config = obtenerConfiguracion();
+
+	if(!$config['habilitado']){
+	    header("Location: /../");
+	}
+
+
+	function crearUsuario($modif=false){
+		//si $modif == true, quiere decir que el usuario ya existe en la bd, si es false, es la primera vez
+		if(isset($_POST['rol'])){
+			$roles = [];
+			for($i=0; $i < count($_POST['rol']); $i++) {
+				array_push($roles,$_POST['rol'][$i]);	
+			}
+			
+			$rolesCompletos = RepositorioRol::getInstance()->buscarRolesPorNombre($roles);
+		}
+		else{
+			$rolesCompletos = null;
+		}
+		if($modif){
+			$creadoEn = $_POST['fechaCreacion'];
+			$actualizadoEn = $_POST['fechaActualizacion'];
+		}
+		else{
+			$now = date("Y-m-d");
+			$creadoEn = $now;
+			$actualizadoEn = $now;
+		}
+		$user =  new Usuario($_POST['usuario'], $_POST['email'], $_POST['password'], true, $creadoEn, $actualizadoEn, $_POST['nombre'], $_POST['apellido'], $rolesCompletos);
+		if($modif) $user->setId($_POST['id']);
+		if(isset($_POST['password2'])) $user->setPassword2($_POST['password2']);
+	
+		return $user;
+		
+	}
+	function usuarioActual(){
+    	if(isset($_SESSION['idUsuario'])){
+    		$usuario = RepositorioUsuario::getInstance()->buscarUsuarioPorId($_SESSION['idUsuario']);
+    		return array(	'logueado'=>true, 
+    						'username'=>$usuario->getNombreUsuario(),
+    						'roles'=>RepositorioRol::getInstance()->buscarRolesDeUsuario($_SESSION['idUsuario']),
+    						'idUsuario'=>$_SESSION['idUsuario'],
+    						'token'=>$_SESSION['token']
+    					);
+    	}
+    	else return false;
+    }
+
+	function listarUsuarios($usuarios,$filtrado=null,$filtradoPaginado=false){
+		$paginado = datosDePaginado();
+		if($filtradoPaginado){
+			$paginado['cantPaginas'] = ceil(sizeof(RepositorioUsuario::getInstance()->devolverUsuarios(0,999,$filtrado))/$paginado['cantidadPorPagina']);
+		}
+		echo TwigView::getTwig()->render('administracionUsuarios.twig', array('usuarioActual'=>usuarioActual(),'lista'=>$usuarios,'filtrado'=>$filtrado,'configuracion'=>obtenerConfiguracion(), 'paginado' =>$paginado,'filtradoPaginado'=>$filtradoPaginado));
+    }
+    function agregarUsuario($validacion=[],$roles){
+		echo TwigView::getTwig()->render('administracionAgregarUsuario.twig', array('usuarioActual'=>usuarioActual(),'validacion'=>$validacion,'roles'=>$roles,'configuracion'=>obtenerConfiguracion()));
+		
+    }
+    function modificacionDeUsuario($usuario,$validacion=[],$roles){
+		echo TwigView::getTwig()->render('administracionModificarUsuario.twig', array('usuarioActual'=>usuarioActual(),'usuario'=>$usuario, 'validacion'=>$validacion,'roles'=>$roles,'configuracion'=>obtenerConfiguracion()));
+    }
+    function loginUsuario($msj=''){
+		echo TwigView::getTwig()->render('loginUsuario.twig', array('usuarioActual'=>usuarioActual(),'mensaje'=>$msj, 'configuracion'=>obtenerConfiguracion()));
+    }
+    /*
+    function usuarioLogueado(){
+	    echo TwigView::getTwig()->render('home.twig', array('configuracion'=>obtenerConfiguracion()));
+    }
+    */
+    function loguearUsuario($usuario){
+    	if(!isset($_SESSION)) {
+			sec_session_start();
+		} else {
+			session_destroy();
+			sec_session_start();
+		}
+		$_SESSION['token'] = md5(uniqid(mt_rand(), true));
+    	$_SESSION['idUsuario'] = $usuario->getId();
+        echo TwigView::getTwig()->render('base.twig.html', array('usuarioActual'=>usuarioActual(),'configuracion'=>obtenerConfiguracion()));
+
+    }
+    function mostrarUsuario($usuario){
+		 echo TwigView::getTwig()->render('administracionMostrarUsuario.twig', array('usuarioActual'=>usuarioActual(),'usuario'=>$usuario, 'configuracion'=>obtenerConfiguracion()));
+    }
+
+    function datosDePaginado(){
+
+    	$cantidadUsuarios = (int)RepositorioUsuario::getInstance()->cantidadDeUsuarios();
+    	$cantidadPorPagina = (int)RepositorioConfiguracion::getInstance()->obtenerDatosDeConfiguracion()->getCantElem();
+    	$cantidadDePaginas = ceil($cantidadUsuarios/$cantidadPorPagina);
+    	$actual = 1;
+    	if(isset($_GET['page'])){
+    		$actual = $_GET['page'];
+    	}
+    	if($actual <= 1){
+    		$limit = 0;
+    	}
+    	else{
+    		$limit = $cantidadPorPagina * ($actual - 1);
+    	}
+    	
+    	$retorno = array(
+    		'cantidadPorPagina' => $cantidadPorPagina,
+    		'cantPaginas' =>  $cantidadDePaginas,
+    		'actual' => $actual,
+    		'limit' => $limit
+    			);
+
+    	return $retorno;
+    }
+
+
+	if(isset($_GET['action'])){	
+		switch($_GET['action']){
+			case 'agregarUsuario':
+				if((RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'usuario_new')) && (isset($_POST['token']) && $_POST['token'] == $_SESSION['token'])) {
+					$u = crearUsuario();
+					$validacion = RepositorioUsuario::getInstance()->usuarioValido($u);
+					if($validacion['ok']){
+						RepositorioUsuario::getInstance()->agregarUsuario($u);
+						header("Location: /../controller/ControllerUsuario.php?action=listarUsuarios");
+					}
+					else{
+						agregarUsuario($validacion,RepositorioRol::getInstance()->devolverRoles());
+					}
+				}
+				else{//QUE ES ESTO
+					echo "Post:     ".$_POST['token'];
+					echo "\nSesion: ".$_SESSION['token'];
+					//header("Location: /../");
+				}
+				break;
+			case "agregarUsuarioView":
+				if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'],'usuario_new')){
+					agregarUsuario("", RepositorioRol::getInstance()->devolverRoles());
+				} else {
+					header("Location: /../");
+				}
+				break;
+			case 'modificarUsuario':
+				if((RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'usuario_update')) && (isset($_POST['token']) && $_POST['token'] == $_SESSION['token'])) {
+					$u = crearUsuario(true);
+					$validacion = $validacion = RepositorioUsuario::getInstance()->usuarioValido($u,true);
+					if($validacion['ok']){
+						$resultado = RepositorioUsuario::getInstance()->modificarUsuario($u);
+						if($resultado){
+							$id = $resultado->getId();
+							header("Location: /../controller/ControllerUsuario.php?action=mostrarUsuario&id=$id");
+						}
+					}
+					else{
+							modificacionDeUsuario($u,$validacion,RepositorioRol::getInstance()->devolverRoles());
+						}
+				} 
+				else {
+					header("Location: /../");
+				}
+				break;
+			case 'modificacionDeUsuario':
+				if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'usuario_update')){
+					$usuario = RepositorioUsuario::getInstance()->buscarUsuarioPorId($_GET['id']);
+			    	modificacionDeUsuario($usuario,"",RepositorioRol::getInstance()->devolverRoles());
+			    } else {
+					header("Location: /../");
+				}
+				break;
+			case 'eliminarUsuario':
+				if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'usuario_destroy')){
+					RepositorioUsuario::getInstance()->eliminarUsuario($_GET['id']);
+					header("Location: /../controller/ControllerUsuario.php?action=listarUsuarios");
+				} else {
+					header("Location: /../");
+				}
+				break;
+			case 'loginUsuario': 
+				if(isset($_POST['email']) && isset($_POST['password'])){
+					if(RepositorioUsuario::getInstance()->existeUsuario($_POST['email'],$_POST['password'])){
+						if(RepositorioUsuario::getInstance()->usuarioActivo($_POST['email'])){
+							loguearUsuario(RepositorioUsuario::getInstance()->buscarUsuarioPorEmail($_POST['email']));
+						}
+						else{
+							loginUsuario("El usuario con el que quiere ingresar esta bloqueado");
+						}
+					}
+					else{
+						loginUsuario("Es posible que no exista el usuario o este ingresando mal la contraseña");
+					}
+				}
+				else{
+					header("Location: /../controller/ControllerUsuario.php?action=loginUsuarioView");
+				}
+				break;
+			case 'mostrarUsuario':
+				if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'usuario_show')){
+					mostrarUsuario(RepositorioUsuario::getInstance()->buscarUsuarioPorId($_GET['id']));
+				} else {
+					header("Location: /../");
+				}
+				break;
+			case 'listarUsuarios':
+				if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'usuario_index')){
+					$paginado = datosDePaginado();
+					listarUsuarios(RepositorioUsuario::getInstance()->devolverUsuarios($paginado['limit'],$paginado['cantidadPorPagina']));
+
+				} else {
+	        		header("Location: /../");
+    			}
+				break;
+			case 'loginUsuarioView':
+				loginUsuario();
+				break;
+			case 'activarUsuario':
+				if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'usuario_update')){
+					RepositorioUsuario::getInstance()->activarUsuario($_GET['id']);
+					header("Location: /../controller/ControllerUsuario.php?action=listarUsuarios");
+				} else {
+	        		header("Location: /../");
+    			}
+				break;
+			case 'desactivarUsuario':
+				if(RepositorioPermiso::getInstance()->usuarioTienePermiso($_SESSION['idUsuario'], 'usuario_update')){
+					RepositorioUsuario::getInstance()->bloquearUsuario($_GET['id']);
+					header("Location: /../controller/ControllerUsuario.php?action=listarUsuarios");
+				} else {
+	        		header("Location: /../");
+    			}
+				break;
+			case 'filtradoUsuario': //TERMINAR
+				$paginado = datosDePaginado();
+				$listado = [];
+				$filtrado['activo'] = isset($_POST['activo']);
+				$filtrado['bloqueado'] = isset($_POST['bloqueado']);
+				$filtrado['campoBuscar'] = "";
+				if(isset($_POST['buscar']) && trim($_POST['buscar']) !=''){
+					$filtrado['campoBuscar'] = $_POST['buscar'];	;
+				}
+				if(isset($_POST['filtrado'])){
+					$_SESSION['filtrado']['activo'] = $filtrado['activo'];
+					$_SESSION['filtrado']['bloqueado'] = $filtrado['bloqueado'];
+					$_SESSION['filtrado']['campoBuscar'] = $filtrado['campoBuscar'];
+				}
+				else{
+					$filtrado['activo'] = $_SESSION['filtrado']['activo'];
+					$filtrado['bloqueado'] = $_SESSION['filtrado']['bloqueado'];
+					$filtrado['campoBuscar'] = $_SESSION['filtrado']['campoBuscar'];
+				}
+				
+
+
+				listarUsuarios(RepositorioUsuario::getInstance()->devolverUsuarios($paginado['limit'],$paginado['cantidadPorPagina'],$filtrado),$filtrado,true);
+			
+				break;
+			case 'cerrarSesion':
+				//if(isset($_GET['token']) && $_GET['token'] == $_SESSION['token']) {
+				sec_session_start();
+				$_SESSION = array();
+				session_destroy();
+				header("Location: /../");
+				//} else {
+				//	header("Location: /../");
+				//}
+				break;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+/*
+				if(isset($GLOBALS['filtrar']) && $GLOBALS['filtrar']){
+					if(isset($GLOBALS['activo'])){
+						$filtrado['activo'] = $GLOBALS['activo'];
+					}
+					if(isset($GLOBALS['bloqueado'])){
+						$filtrado['bloqueado'] = $GLOBALS['bloqueado'];
+					}
+					if(isset($GLOBALS['campoBuscar'])){
+						$filtrado['campoBuscar'] = $GLOBALS['campoBuscar'];
+					}
+				}
+				else{
+					$GLOBALS['filtrar'] = 1;
+					$GLOBALS['activo'] = $filtrado['activo'];
+					$GLOBALS['bloqueado'] = $filtrado['bloqueado'];
+					$GLOBALS['campoBuscar'] = $filtrado['campoBuscar'];
+	
+				}
+				var_dump($GLOBALS['filtrar']);
+				var_dump($GLOBALS['activo']);
+				var_dump($GLOBALS['bloqueado']);
+				var_dump($GLOBALS['campoBuscar']);
+
+*/
+
+?>
