5d613f1dacfe41434976a557ebfb8364e7c5525c
Juan Schällibaum <juanschallibaum@hotmail.com>
2017-12-10 06:21:21 -0300

    Arreglando JSON turnos

--- slim.php
+++ slim.php
@@ -20,6 +20,14 @@ $app->get('/turnos/{fecha}', function ($request, $response, $args) {
 	$fecha = explode('-',$fecha);
 	if(preg_match('/^([0-2][0-9]|3[0-1])-(0[1-9]|1[012])-[0-9]{4}$/', $args['fecha']) && checkdate($fecha[1], $fecha[0], $fecha[2])) {
 	//if(preg_match('/^(?:(?:31-(?:0?[13578]|1[02]))\1|(?:(?:29|30)-(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29-0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])-(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/', $args['fecha'])) {
+		$today = new DateTime(date("d-m-Y"));
+		$date = $args['fecha'];
+		$date = new DateTime($date);
+		if ($date < $today) {
+			$res = json_encode(array("code" => 400, "mensaje" => "Bad request: La fecha del turno solicitado ya pasó"), JSON_UNESCAPED_UNICODE);
+			return $response->withStatus(400)->write($res);
+		}
+
 		$todosLosTurnos = array('08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30');
 		$turnosReservados = RepositorioTurno::getInstance()->turnosReservadosParaFecha($args['fecha']);
 	    $turnosDisponibles = array_diff_assoc($todosLosTurnos, $turnosReservados);
