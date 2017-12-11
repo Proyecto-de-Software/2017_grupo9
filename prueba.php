3a14beb38711d97b96600274a461d986d4cd6941
Juan Schällibaum <juanschallibaum@hotmail.com>
2017-12-11 00:56:54 -0300

    Emprilijando

--- prueba.php
+++
@@ -1,6 +0,0 @@
-<?php
-$datetime1 = new DateTime('2011-10-01');
-$datetime2 = new DateTime('2011-12-01');
-$interval = $datetime1->diff($datetime2);
-echo floor(($interval->format('%a') / 7)) . ' semanas';
-?>
--- test-json.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$rawData = file_get_contents("https://api-referencias.proyecto2017.linti.unlp.edu.ar/tipo-vivienda");
-$response = json_decode($rawData, true);
-echo $response['id']['nombre'];
-
-?>
