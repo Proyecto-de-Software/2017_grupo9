ed8b6bf5cd57dd1f7f0eaba030b4d8434ab8ff1d
Nacho Zampelunghe <nzampelunghe@gmail.com>
2017-12-11 01:57:12 -0300

    As

--- controller/ControllerHistoriaClinica.php
+++ controller/ControllerHistoriaClinica.php
@@ -33,6 +33,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/model/RepositorioPermiso.php');
 			if($this->hayPermiso('control_new') || $this->hayPermiso('control_update')){
 				$_SESSION['controlAModificar'] = $idControl;
 				$_SESSION['pacienteControlAModificar'] = $idPaciente;
+				var_dump($paciente);die();
 				$edad = $this->calcularEdad(RepositorioPaciente::getInstance()->buscarPorId($idPaciente)->getFechaNacimiento());
 				$parametrosTemplate = $argsTmp;
 				$parametrosTemplate['edad'] = $edad;
