c6fd73366226e4b578864fb1c046fdb6fc351cf0
Nacho Zampelunghe <nzampelunghe@gmail.com>
2017-12-11 01:48:53 -0300

    As

--- controller/ControllerHistoriaClinica.php
+++ controller/ControllerHistoriaClinica.php
@@ -31,8 +31,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/model/RepositorioPermiso.php');
 
 		public function formulario($idPaciente,$argsTmp = [], $idControl = null){ //new
 			if($this->hayPermiso('control_new') || $this->hayPermiso('control_update')){
-				$_SESSION['controlAModificar'] == $idControl;
-				$_SESSION['pacienteControlAModificar'] == $idPaciente;
+				$_SESSION['controlAModificar'] = $idControl;
+				$_SESSION['pacienteControlAModificar'] = $idPaciente;
 				$edad = $this->calcularEdad(RepositorioPaciente::getInstance()->buscarPorId($idPaciente)->getFechaNacimiento());
 				$parametrosTemplate = $argsTmp;
 				$parametrosTemplate['edad'] = $edad;
