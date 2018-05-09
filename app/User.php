b9d107a9d97527b44f298e205000b76111d59e6b
BRAÑEIRO, GABRIEL OMAR <gabbraneiro@gmail.com>
2018-05-09 10:02:14 -0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -30,7 +30,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    public function roles(){
-        return $this->belongsToMany('\App\Role','role_user');
-    }
+
 }
