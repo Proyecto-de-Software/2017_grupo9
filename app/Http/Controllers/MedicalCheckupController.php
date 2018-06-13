9336d39fe9e60d6953b23c266fb4019332512e4e
nzampelunghe <nzampelunghe@gmail.com>
2018-06-12 23:50:24 -0300

    as

--- app/Http/Controllers/MedicalCheckupController.php
+++ app/Http/Controllers/MedicalCheckupController.php
@@ -27,7 +27,7 @@ class MedicalCheckupController extends Controller
             return redirect()->route('home'); 
         }
         $controls = MedicalCheckup::where('patient_id', '=', $id)->get();
-        return view('medicalCheckups.index', compact('controls'))->with('patient_id',$id)->with('username',$username)->with('config',$this->getConfiguration());
+        return view('medicalCheckups.index', compact('controls'))->with('patient_id',$id)->with('config',$this->getConfiguration());
     }
 
     /**
